51dd2070f924ac275687930c434e1bc8da74e3f3
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-12-18 21:30:41 +0600

    user account creation and manage api done, alhamdulillah

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ const client = new MongoClient(uri, {
   }
 })
 
+// all collections
 const DB = client.db('GET_IT_DONE')
 const serviceCollection = DB.collection('services')
+const userCollection = DB.collection('users')
 
 async function run () {
   try {
@@ -34,25 +36,99 @@ async function run () {
     // await client.db('admin').command({ ping: 1 })
     console.log('Pinged your deployment. Successfully connected to MongoDB!')
 
+    //get all services or by page
     app.get('/services', async (req, res) => {
       const limit = parseInt(req.query.limit)
       const page = parseInt(req.query.page) || 1
-
-      if (limit > 0) {
-        const skip = (page - 1) * limit
-        const totalCount = await serviceCollection.countDocuments()
-        const totalPages = Math.ceil(totalCount/limit)
-        const result = await serviceCollection
-          .find()
-          .skip(skip)
-          .limit(limit)
-          .toArray()
-        res.send({ result, totalCount, totalPages })
-      } else {
-        const result = await serviceCollection.find().toArray()
-        res.send([result.length, result])
+      const totalCount = await serviceCollection.countDocuments()
+      try {
+        if (limit > 0) {
+          const skip = (page - 1) * limit
+          const totalPages = Math.ceil(totalCount / limit)
+          const result = await serviceCollection
+            .find()
+            .skip(skip)
+            .limit(limit)
+            .toArray()
+          res.status(200).send({ result, totalCount, totalPages })
+        } else {
+          const result = await serviceCollection.find().toArray()
+          res.status(200).send({ services: result, totalCount })
+        }
+      } catch (error) {
+        res.status(500).send({ message: 'Server Error' })
       }
     })
+
+    // post a new user
+    app.post('/user', async (req, res) => {
+      const {
+        displayName,
+        email,
+        photoURL,
+        phoneNumber,
+        createdAt,
+        lastLoginAt,
+        password
+      } = req.body
+      try {
+        const existingUser = await userCollection.findOne({ email })
+        // console.log(updatedUser)
+        const usersCount = await userCollection.countDocuments()
+        let updatedUser
+        // if the user is New
+        if (!existingUser) {
+          updatedUser = {
+            id: usersCount + 1,
+            name: displayName,
+            email,
+            isAvailable: true,
+            title: '',
+            bio: '',
+            photoURL,
+            coverPhotoURL: '',
+            phoneNumber,
+            address: '',
+            fbAddress: '',
+            linkedInAddress: '',
+            twitterAddress: '',
+            isVerified: false,
+            createdAt,
+            loggedInData: [lastLoginAt],
+            services: [],
+            bookedServices: [],
+            earningHistory: [],
+            spendingHistory: [],
+            password
+          }
+          const result = await userCollection.insertOne(updatedUser)
+          res.status(200).send(result)
+        } else {
+          updatedUser = {
+            ...existingUser,
+            name: existingUser.name || displayName,
+            photoURL: existingUser.photoURL || photoURL,
+            createdAt: createdAt,
+            loggedInData: [...existingUser.loggedInData, lastLoginAt],
+            password: existingUser.password || password
+          }
+          const filter = { email }
+          const updatedDoc = { $set: updatedUser }
+          const options = { upsert: true }
+          const result = await userCollection.updateOne(
+            filter,
+            updatedDoc,
+            options
+          )
+          res.status(200).send(result)
+        }
+      } catch (err) {
+        res.status(500).send({ err, message: 'server error' })
+      }
+    })
+
+    // patch with previous user or create new one
+    app.patch('/user', async (req, res) => {})
   } finally {
     // Uncomment this line if you want to keep the connection open
     // await client.close();
